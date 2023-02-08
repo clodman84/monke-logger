4a4d7fc56df09d845e4433eb66c555bffcd809cd
clodman84 <cshourin@gmail.com>
2023-02-08 14:02:17 +0530

    Added create_theme frame

--- gui.py
+++ gui.py
@@ -4,6 +4,18 @@ from tkinter import ttk
 import database
 
 
+class RecordEntry(tk.Frame):
+    def __init__(self, *args, theme, **kwargs):
+        super(RecordEntry, self).__init__(*args, **kwargs)
+        self.theme = theme
+
+
+class BenchmarkEntry(tk.Frame):
+    def __init__(self, *args, theme, **kwargs):
+        super(BenchmarkEntry, self).__init__(*args, **kwargs)
+        self.theme = theme
+
+
 class BenchmarkTable(tk.Frame):
     def __init__(self, *args, theme, **kwargs):
         super(BenchmarkTable, self).__init__(*args, **kwargs)
@@ -19,6 +31,9 @@ class BenchmarkTable(tk.Frame):
 
     def refresh(self):
         data: list[list[database.Benchmark]] = database.view_benchmarks(self.theme)
+        if len(data) == 0:
+            return
+
         self.tree["columns"] = ("date", "value")
 
         self.tree.column("date", width=120, anchor="w")
@@ -64,6 +79,8 @@ class RecordTable(tk.Frame):
 
     def refresh(self):
         data = database.view_records(self.theme)
+        if len(data) == 0:
+            return
         sample: database.Record = data[0]
 
         # creating the headings and all that
@@ -105,10 +122,41 @@ class App(tk.Frame):
 def main():
     root = tk.Tk()
     note_book = ttk.Notebook(master=root)
-    for theme in database.view_themes():
+
+    themes = database.view_themes()
+    for theme in themes:
         page = Page(master=note_book, theme=theme)
         page.grid(column=0, row=0, sticky="ew")
         note_book.add(page, text=theme.capitalize())
+
+    new_theme_frame = tk.Frame(note_book)
+    label = tk.Label(new_theme_frame, text="Enter Theme: ")
+    text_box = tk.Entry(new_theme_frame)
+    text_box.grid(column=1, row=0)
+    label.grid(column=0, row=0)
+
+    # Create the application variable.
+    contents = tk.StringVar()
+    # Tell the entry widget to watch this variable.
+    text_box["textvariable"] = contents
+
+    def make_theme():
+        requested_theme = contents.get()
+        if requested_theme.lower() in themes:
+            return
+        # register the theme in the database
+        database.create_theme(requested_theme)
+
+        # add the theme to the list so that it can't be made again
+        themes.append(requested_theme)
+
+        # update the notebook with a page for the theme
+        theme_page = Page(master=note_book, theme=requested_theme)
+        note_book.insert(0, theme_page, text=requested_theme.capitalize())
+
+    tk.Button(new_theme_frame, command=make_theme, text="Create").grid(column=3, row=0)
+    note_book.add(new_theme_frame, text="+")
+
     note_book.grid(column=0, row=0, sticky="ew")
     root.resizable(False, False)
     root.mainloop()
